55db017d4592d783bd4aebff80054d6df5deb7e2
ssmith <stephanelsmith@ymail>
2025-09-07 11:47:34 -0500

    tinyc6: added neopixel code

--- board.py
+++
@@ -1,6 +0,0 @@
-
-# I2C pins
-I2C_SDA  = const(41)
-I2C_SCL  = const(42)
-I2C_FREQ = const(400000) # fast mode
-
---
+++ src/board.py
@@ -0,0 +1,11 @@
+
+# I2C pins
+I2C_SDA  = const(6)
+I2C_SCL  = const(7)
+I2C_FREQ = const(400000) # fast mode
+
+# LEDS
+LED_PWR_EN = const(22)
+LED_DAT    = const(23)
+LED_LENGTH = const(1)
+
